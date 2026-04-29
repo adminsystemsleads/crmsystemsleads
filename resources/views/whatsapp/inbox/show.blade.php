d125a4f3fc73443eab69a707397e0121d0fb44ea
adminsystemsleads <admin@systemsleads.com>
2026-04-28 19:14:41 -0500

    crm3

--- resources/views/whatsapp/inbox/show.blade.php
+++ resources/views/whatsapp/inbox/show.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
 
 @php
-  use Illuminate\Support\Str;
   $avatarColors = ['bg-green-100 text-green-700','bg-blue-100 text-blue-700','bg-purple-100 text-purple-700',
                    'bg-amber-100 text-amber-700','bg-rose-100 text-rose-700','bg-teal-100 text-teal-700'];
   function waAvatar(string $name, array $colors): string {
