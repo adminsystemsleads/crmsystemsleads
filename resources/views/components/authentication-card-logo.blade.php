d2188853a91f0c452b2c37a25e7ad170126b6bb0
adminsystemsleads <admin@systemsleads.com>
2026-05-28 16:42:10 -0500

    logo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" class="h-20 w-auto">
+    <img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" class="h-[4.5rem] w-auto">
 </a>
