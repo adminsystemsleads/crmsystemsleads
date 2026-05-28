e6496b76ccd34c2f0791ddd6a585e7f9d25286e4
adminsystemsleads <admin@systemsleads.com>
2026-05-28 16:34:36 -0500

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" {{ $attributes->merge(['class' => 'h-[1.8rem] w-auto']) }}>
+<img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" {{ $attributes->merge(['class' => 'h-[0.9rem] w-auto']) }}>
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" class="h-[2.4rem] w-auto">
+    <img src="{{ asset('logo_1.png') }}" alt="{{ config('app.name', 'Logo') }}" class="h-[1.2rem] w-auto">
 </a>
